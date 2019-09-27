1193d6f92b477b7ec79a6c65b87ee771360727c5
Jonas <info@csharpschool.com>
2019-09-27 11:45:13 +0200

    Adding a Construcor to the VODContext Class

--- VOD.Database/Contexts/VODContext.cs
+++ VOD.Database/Contexts/VODContext.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Text;
 using VOD.Common.Entities;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace VOD.Database.Contexts
 {
     public class VODContext : IdentityDbContext<VODUser>
     {
+        public VODContext(DbContextOptions<VODContext> options) : base(options)
+        {
+        }
+
     }
 }
