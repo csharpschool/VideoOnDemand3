c5625679215cd119a6a1bf0c66590beba7d6452f
Jonas <info@csharpschool.com>
2019-09-27 14:46:30 +0200

    Adding the GetSync Method to the IDbReadService Interface

--- VOD.Database/Services/IDbReadService.cs
+++ VOD.Database/Services/IDbReadService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,6 @@ namespace VOD.Database.Services
     public interface IDbReadService
     {
         Task<List<TEntity>> GetAsync<TEntity>() where TEntity : class;
+        Task<List<TEntity>> GetAsync<TEntity>(Expression<Func<TEntity, bool>> expression) where TEntity : class;
     }
 }
