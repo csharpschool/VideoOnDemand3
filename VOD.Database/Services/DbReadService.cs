1875a85e2ce00320759de70e62aa51f820b1599d
Jonas <info@csharpschool.com>
2019-09-27 14:43:20 +0200

    Adding the Service Interface and Class

---
+++ VOD.Database/Services/DbReadService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VOD.Database.Contexts;
+
+namespace VOD.Database.Services
+{
+    public class DbReadService : IDbReadService
+    {
+        private VODContext _db;
+        public DbReadService(VODContext db)
+        {
+            _db = db;
+        }
+
+    }
+}
