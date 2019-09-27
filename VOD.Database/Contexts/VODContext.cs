615d83d36c05c29a670b902a3c7e657211f789c0
Jonas <info@csharpschool.com>
2019-09-27 11:44:33 +0200

    Creating the Database Context Class

---
+++ VOD.Database/Contexts/VODContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VOD.Common.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace VOD.Database.Contexts
+{
+    public class VODContext : IdentityDbContext<VODUser>
+    {
+    }
+}
