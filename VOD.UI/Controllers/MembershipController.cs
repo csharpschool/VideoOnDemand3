5b13c07ce505836200c58c9d9663bb70636a4c7d
Jonas <info@csharpschool.com>
2019-09-27 15:48:30 +0200

    Adding the Membership Controller

---
+++ VOD.UI/Controllers/MembershipController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VOD.UI.Controllers
+{
+    public class MembershipController : Controller
+    {
+        [HttpGet]
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Course(int id)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Video(int id)
+        {
+            return View();
+        }
+
+    }
+}
