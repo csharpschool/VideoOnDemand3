d8049720519257cbd5c92e405a5ccaf6653bd532
Jonas <info@csharpschool.com>
2019-09-27 14:51:09 +0200

    Adding the AnySync Method to the IDbReadService Interface

--- VOD.Database/Services/IDbReadService.cs
+++ VOD.Database/Services/IDbReadService.cs
@@ -11,6 +11,7 @@ namespace VOD.Database.Services
         Task<List<TEntity>> GetAsync<TEntity>() where TEntity : class;
         Task<List<TEntity>> GetAsync<TEntity>(Expression<Func<TEntity, bool>> expression) where TEntity : class;
         Task<TEntity> SingleAsync<TEntity>(Expression<Func<TEntity, bool>> expression) where TEntity : class;
+        Task<bool> AnyAsync<TEntity>(Expression<Func<TEntity, bool>> expression) where TEntity : class;
 
     }
 }
