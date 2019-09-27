6ded2e759ed77fd6ea059fcea44908f4c246455d
Jonas <info@csharpschool.com>
2019-09-27 13:44:24 +0200

    Adding the Download DTO

---
+++ VOD.Common/DTOModels/DownloadDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VOD.Common.DTOModels
+{
+    public class DownloadDTO
+    {
+        public string DownloadUrl { get; set; }
+        public string DownloadTitle { get; set; }
+    }
+}
