10d76597886848176154d61d1ee802fbc10bff82
Jonas <info@csharpschool.com>
2019-09-27 19:10:48 +0200

    Adding a Constructor

--- VOD.Admin/TagHelpers/BtnTagHelper.cs
+++ VOD.Admin/TagHelpers/BtnTagHelper.cs
@@ -2,15 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Razor.Runtime.TagHelpers;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 
 namespace VOD.Admin.TagHelpers
 {
     // You may need to install the Microsoft.AspNetCore.Razor.Runtime package into your project
-    [HtmlTargetElement("tag-name")]
-    public class BtnTagHelper : TagHelper
+    [HtmlTargetElement("btn")]
+    public class BtnTagHelper : AnchorTagHelper
     {
+        public BtnTagHelper(IHtmlGenerator generator) : base(generator)
+        {
+        }
+
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
 
