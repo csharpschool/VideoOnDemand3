f693f62f505bc9e9e9aa39182d6d708903ca1fbf
Jonas <info@csharpschool.com>
2019-09-27 14:08:46 +0200

    Adding the SeedMembershipData method

--- VOD.Database/Contexts/VODContext.cs
+++ VOD.Database/Contexts/VODContext.cs
@@ -89,5 +89,12 @@ namespace VOD.Database.Contexts
             });
             #endregion
         }
+
+        public void SeedMembershipData()
+        {
+            #region Lorem Ipsum - Dummy Data
+            var description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+            #endregion
+        }
     }
 }
