b2eda9863a7dea01eb588acf04fbf2039a3c3bdd
Jonas <info@csharpschool.com>
2019-09-27 13:43:46 +0200

    Adding the Course DTO

---
+++ VOD.Common/DTOModels/CourseDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VOD.Common.DTOModels
+{
+    public class CourseDTO
+    {
+        public int CourseId { get; set; }
+        public string CourseTitle { get; set; }
+        public string CourseDescription { get; set; }
+        public string MarqueeImageUrl { get; set; }
+        public string CourseImageUrl { get; set; }
+    }
+}
