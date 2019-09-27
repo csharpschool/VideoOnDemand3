835aa8b48ddd099da4e7fa4a8ae26a2bdf9e4ed0
Jonas <info@csharpschool.com>
2019-09-27 19:07:08 +0200

    Creating the <btn> Tag Helper

--- VOD.Admin/TagHelpers/BtnTagHelper.cs
+++ VOD.Admin/TagHelpers/BtnTagHelper.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc.TagHelpers;
 using Microsoft.AspNetCore.Razor.Runtime.TagHelpers;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 
@@ -10,7 +9,7 @@ namespace VOD.Admin.TagHelpers
 {
     // You may need to install the Microsoft.AspNetCore.Razor.Runtime package into your project
     [HtmlTargetElement("tag-name")]
-    public class BtnTagHelper : AnchorTagHelper
+    public class BtnTagHelper : TagHelper
     {
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
