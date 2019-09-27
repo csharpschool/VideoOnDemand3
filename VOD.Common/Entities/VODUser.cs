2056056ceb42bbcff42435e6dbf00defc57ddb9a
Jonas <info@csharpschool.com>
2019-09-27 11:40:33 +0200

    Adding the VODUser Class

---
+++ VOD.Common/Entities/VODUser.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+namespace VOD.Common.Entities
+{
+    public class VODUser : IdentityUser
+    {
+    }
+}
