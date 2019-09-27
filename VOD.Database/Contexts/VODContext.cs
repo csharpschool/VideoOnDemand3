816186545a2c312481c149faed16a04f2d4a9a45
Jonas <info@csharpschool.com>
2019-09-27 14:10:27 +0200

    Fetching a User

--- VOD.Database/Contexts/VODContext.cs
+++ VOD.Database/Contexts/VODContext.cs
@@ -95,6 +95,17 @@ namespace VOD.Database.Contexts
             #region Lorem Ipsum - Dummy Data
             var description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
             #endregion
+
+            #region Fetch a User
+            var email = "a@b.c";
+            var userId = string.Empty;
+
+            if (Users.Any(r => r.Email.Equals(email)))
+                userId = Users.First(r => r.Email.Equals(email)).Id;
+            else
+                return;
+            #endregion
+
         }
     }
 }
