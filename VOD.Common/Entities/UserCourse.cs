576e0412e719d0242426afb281da09f7f7addd85
Jonas <info@csharpschool.com>
2019-09-27 13:58:08 +0200

    The UserCourse Entity

---
+++ VOD.Common/Entities/UserCourse.cs
@@ -0,0 +1,10 @@
+﻿namespace VOD.Common.Entities
+{
+    public class UserCourse
+    {
+        public string UserId { get; set; }
+        public VODUser User { get; set; }
+        public int CourseId { get; set; }
+        public Course Course { get; set; }
+    }
+}
