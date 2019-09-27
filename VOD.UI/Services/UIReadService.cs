c40c20c632d2f52e2b3c1820e67bec7ef1ca85ac
Jonas <info@csharpschool.com>
2019-09-27 15:27:43 +0200

    Injecting the IDbReadService Into the UIReadService Class

--- VOD.UI/Services/UIReadService.cs
+++ VOD.UI/Services/UIReadService.cs
@@ -2,10 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using VOD.Database.Services;
 
 namespace VOD.UI.Services
 {
     public class UIReadService : IUIReadService
     {
+        #region Properties
+        private readonly IDbReadService _db;
+        #endregion
+
+        #region Constructor
+        public UIReadService(IDbReadService db)
+        {
+            _db = db;
+        }
+        #endregion
+
+        #region Methods
+        #endregion
+
     }
 }
