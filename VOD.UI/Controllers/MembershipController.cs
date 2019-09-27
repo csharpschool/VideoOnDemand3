f85b360925def92cbf27693a211f812e0c1ad9eb
Jonas <info@csharpschool.com>
2019-09-27 15:49:46 +0200

    Injecting the IHttpContextAccessor Service

--- VOD.UI/Controllers/MembershipController.cs
+++ VOD.UI/Controllers/MembershipController.cs
@@ -2,12 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using VOD.Common.Entities;
+using VOD.UI.Services;
 
 namespace VOD.UI.Controllers
 {
     public class MembershipController : Controller
     {
+        private readonly string _userId;
+        private readonly IMapper _mapper;
+        private readonly IUIReadService _db;
+
+        public MembershipController(
+        IHttpContextAccessor httpContextAccessor,
+        UserManager<VODUser> userManager, IMapper mapper, IUIReadService db)
+        {
+            var user = httpContextAccessor.HttpContext.User;
+            _userId = userManager.GetUserId(user);
+            _mapper = mapper;
+            _db = db;
+        }
+
         [HttpGet]
         public IActionResult Dashboard()
         {
