22a3dfd524b67566f8053db8852759f520ce169e
Jonas <info@csharpschool.com>
2019-09-27 14:44:08 +0200

    Regitering the Service in the Startup Class

--- VOD.UI/Startup.cs
+++ VOD.UI/Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using VOD.Database.Contexts;
 using VOD.Common.Entities;
+using VOD.Database.Services;
 
 namespace VOD.UI
 {
@@ -35,6 +36,8 @@ namespace VOD.UI
                 .AddEntityFrameworkStores<VODContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
+
+            services.AddScoped<IDbReadService, DbReadService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
