100cc1bc1ceafdd919aed9277eab0cba1b504f17
Jonas <info@csharpschool.com>
2019-10-05 15:33:23 +0200

    Replaced wrong NuGet package for SelectList

--- VOD.Common/Extensions/ListExtensions.cs
+++ VOD.Common/Extensions/ListExtensions.cs
@@ -1,7 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 using System.Collections.Generic;
 using System.Text;
-using System.Web.Mvc;
 
 namespace VOD.Common.Extensions
 {
